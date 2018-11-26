b367b1ddd9e72078edcb4cc4cc4095e503309a77
Robert Thomas <robert.thomas@arcruy.com>
2018-11-26 15:56:02 +0000

    Plexus v0.9.35
    - Added better error handling to the list function
    - Added better logging to the list function

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-printf "\\e[93m=== Plexus v0.9.34 - Developed by Robert Thomas ==="
+printf "\\e[93m=== Plexus v0.9.35 - Developed by Robert Thomas ==="
 printf "\\n=== https://github.com/Wolveix/Plexus ==="
 printf "\\n\\e[36mInstalling any missing dependencies.\\n\\e[94m"
 apt-get install curl ffmpeg -f -y -qq
