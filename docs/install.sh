da285109fcf1ef52b8434a711b384bc0e94a9b56
Robert Thomas <robert.thomas@arcruy.com>
2019-01-30 18:11:12 +0000

    Plexus v0.9.51 - Massively improved scan speed during extension check
    - Added -q and -y flags to a yum install call within the install script (thanks @gorgarp!)
    - Added file verification to the list function as an error could occur if a directory was being checked instead of a file (thanks @gorgarp!)
    - Seperated media container check from codec checks within the list function, this drastically speeds up the list function when using remote media

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-printf "\\e[93m=== Plexus v0.9.50 - Developed by Robert Thomas ==="
+printf "\\e[93m=== Plexus v0.9.51 - Developed by Robert Thomas ==="
 printf "\\n=== https://github.com/Wolveix/Plexus ===\\e[0m"
 distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 case $distro in
@@ -7,7 +7,7 @@ case $distro in
     printf "\\n\\e[36mInstalling any missing dependencies.\\n\\e[94m"
     yum -q -y install epel-release
     yum -q -y update && yum -q -y upgrade
-    yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
+    yum -q -y localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
     yum -q -y install curl ffmpeg ffmpeg-devel progress
     ;;
   '"Debian GNU/Linux"' | '"Ubuntu"')
